f534a0aaa0947ef889f8b5ad181fc1229b8629e3
korbek100 <korbek100@gmail.com>
2018-01-09 00:05:19 -0300

    Initial commit

---
+++ 003Recorder_ios_ObjC/ViewController.h
@@ -0,0 +1,34 @@
+//
+//  ViewController.h
+//  003Recorder_ios_ObjC
+//
+//  Created by Henry AT on 9/1/15.
+//  Copyright (c) 2015 Apps4s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface ViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *startStopButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+
+
+- (IBAction)startStopButtonPressed:(id)sender;
+- (IBAction)playButtonPressed:(id)sender;
+
+@property (nonatomic, strong) NSURL *tmpFile;
+@property (nonatomic, strong) AVAudioRecorder *recorder;
+@property BOOL recording;
+
+
+
+
+
+
+
+@end
+
---
+++ 003Recorder_ios_ObjC/ViewController.m
@@ -0,0 +1,107 @@
+//
+//  ViewController.m
+//  003Recorder_ios_ObjC
+//
+//  Created by Henry AT on 9/1/15.
+//  Copyright (c) 2015 Apps4s. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <CoreAudio/CoreAudioTypes.h>
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize recording;
+@synthesize recorder;
+@synthesize tmpFile;
+@synthesize startStopButton;
+@synthesize playButton;
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  recording = NO;
+  
+  
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startStopButtonPressed:(id)sender {
+  AVAudioSession * audioSession = [AVAudioSession sharedInstance];
+  
+  if (!recording) {
+    
+    recording = YES;
+    
+    [audioSession setCategory:AVAudioSessionCategoryRecord error:nil];
+    [audioSession setActive:YES error:nil];
+    
+    
+    [startStopButton setTitle:@"Stop Recording" forState:UIControlStateNormal];
+    
+    NSMutableDictionary* recordSetting = [[NSMutableDictionary alloc] init];
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatAppleIMA4] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    tmpFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent: [NSString stringWithFormat: @"%.0f.%@", [NSDate timeIntervalSinceReferenceDate] * 1000.0, @"caf"]]];
+    
+    recorder = [[AVAudioRecorder alloc] initWithURL:tmpFile settings:recordSetting error:nil];
+    [recorder setDelegate:self];
+    [recorder prepareToRecord];
+    [recorder record];
+    
+  } else {
+    
+    recording = NO;
+    // Sugerencia de Console AudioSessio will be Stop:
+
+
+    [audioSession setActive:NO error:nil];
+    [startStopButton setTitle:@"Start Recording" forState:UIControlStateNormal];
+    [recorder stop];
+    
+  }
+  
+  
+}
+
+
+
+- (IBAction)playButtonPressed:(id)sender {
+  AVAudioSession * audioSession = [AVAudioSession sharedInstance];
+  [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
+  [audioSession setActive:YES error:nil];
+  
+  AVAudioPlayer * player = [[AVAudioPlayer alloc] initWithContentsOfURL:tmpFile error:nil];
+  [player prepareToPlay];
+  [player play];
+  
+}
+
+
+
+
+
+// According StackOverFlow
+
+
+-(void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+  [player stop];
+  [player prepareToPlay];
+}
+
+
+
+
+@end
